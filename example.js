458726eec25d2ae5c73ebb9fbde3d09742af5288
Andrew Kachnic <ajkachnic@protonmail.com>
2020-12-01 23:07:44 -0500

    fixed bug cause i'm stupid

---
+++ example.js
@@ -0,0 +1,3 @@
+const {template} = require('.')
+
+console.log(template('hello world'))
--- index.js
+++ index.js
@@ -1,1 +1,1 @@
-module.exports={template=(s,o={},t=false)=>{let m=s.match(/{{ *[a-zA-Z_.]+ *}}/g);m && m.forEach(m=> s = s.replace(m,o[m.slice(2,-2).trim()]|| t ? eval(`throw Error('${m} not found')`) : m));return s}}
+module.exports={template:(s,o={},t=false)=>{m=s.match(/{{ *[a-zA-Z_.]+ *}}/g);m&&m.forEach(m=>s=s.replace(m,o[m.slice(2,-2).trim()]||t?eval(`throw Error('${ m } not found')`):m));return s}};
